b97c29e974b3db6ee8247dda60264a2f39e83687
Chris Gambrell <christopher@gambrell.info>
2024-10-23 11:23:58 -0400

    replace header to make it a team switcher

--- app/(protected)/components/header.tsx
+++ app/(protected)/components/header.tsx
@@ -1,24 +1,59 @@
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuShortcut,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 import { SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar'
 import { MOCK_APP_ICON, MOCK_APP_NAME } from '@/lib/constants'
-import Link from 'next/link'
+import { ChevronsUpDownIcon, PlusIcon } from 'lucide-react'
 
 export function Header() {
 	return (
 		<SidebarHeader>
 			<SidebarMenu>
 				<SidebarMenuItem>
-					<SidebarMenuButton size='lg' asChild>
-						<Link href='/'>
-							<div className='flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground'>
-								<MOCK_APP_ICON className='size-4' />
-							</div>
-							<div className='grid flex-1 text-left text-sm leading-tight'>
-								<span className='truncate font-semibold'>{MOCK_APP_NAME}</span>
-								{/* TODO: Plan name */}
-								<span className='truncate text-xs'>Free</span>
-							</div>
-						</Link>
-					</SidebarMenuButton>
+					<DropdownMenu>
+						<DropdownMenuTrigger asChild>
+							<SidebarMenuButton
+								size='lg'
+								className='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'>
+								<div className='flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground'>
+									<MOCK_APP_ICON className='size-4' />
+								</div>
+								<div className='grid flex-1 text-left text-sm leading-tight'>
+									<span className='truncate font-semibold'>{MOCK_APP_NAME}</span>
+									{/* TODO: The actual plan */}
+									<span className='truncate text-xs'>Free</span>
+								</div>
+								<ChevronsUpDownIcon className='ml-auto' />
+							</SidebarMenuButton>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent
+							className='w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg'
+							align='start'
+							side='bottom'
+							sideOffset={4}>
+							<DropdownMenuLabel className='text-xs text-muted-foreground'>Teams</DropdownMenuLabel>
+							<DropdownMenuItem className='gap-2 p-2'>
+								<div className='flex size-6 items-center justify-center rounded-sm border'>
+									<MOCK_APP_ICON className='size-4 shrink-0' />
+								</div>
+								{MOCK_APP_NAME}
+								<DropdownMenuShortcut>⌘1</DropdownMenuShortcut>
+							</DropdownMenuItem>
+							<DropdownMenuSeparator />
+							<DropdownMenuItem className='gap-2 p-2'>
+								<div className='flex size-6 items-center justify-center rounded-md border bg-background'>
+									<PlusIcon className='size-4' />
+								</div>
+								<div className='font-medium text-muted-foreground'>Add team</div>
+							</DropdownMenuItem>
+						</DropdownMenuContent>
+					</DropdownMenu>
 				</SidebarMenuItem>
 			</SidebarMenu>
 		</SidebarHeader>
